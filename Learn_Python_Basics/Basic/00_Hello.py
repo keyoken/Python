efb498167285f4938ec07b51e286701fee1901bd
keyoronin <87255632+keyoken@users.noreply.github.com>
2022-10-19 18:17:10 -0500

    Add files via upload

--- Learn_Python_Basics/Basic/00_Hello.py
+++ Learn_Python_Basics/Basic/00_Hello.py
@@ -1,1 +1,45 @@
+# Hola mundo
 
+print("Hola Python")
+print('Hola Python')
+#print(Hola Python)
+  
+
+"""Este es un 
+    comentatio
+    de varias lineas"""
+
+
+
+ ##Consulta lipo de dato
+
+print(type("Hola Python")) #tipo str
+print(type(5)) #tipo int
+print(type(5.98)) #tipo float 
+print(type(3 + 1j)) #tipo complex     
+print(type(True)) #tipo  bool
+
+
+# Introduction
+# Day 1 - 30DaysOfPython Challenge
+
+print(2 + 3)   # addition(+)
+print(3 - 1)   # subtraction(-)
+print(2 * 3)   # multiplication(*)
+print(3 / 2)   # division(/)
+print(3 ** 2)  # exponential(**)
+print(3 % 2)   # modulus(%)
+print(3 // 2)  # Floor division operator(//)
+
+# Checking data types
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Asabeneh'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+print(type(print("Mi cadena de texto")))  # Tipo 'NoneType'
